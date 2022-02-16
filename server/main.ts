82ec07ae79cb2788fab26c9ad6719cf65e1138c8
Elijah Mooring <vehmloewff@gmail.com>
2022-02-16 09:39:59 -0600

    always support CORS, even when method errors out

--- server/main.ts
+++ server/main.ts
@@ -6,23 +6,37 @@ import './register-controllers.ts'
 import './connection.ts'
 import './bypass/mod.ts'
 
+const defaultHeaders = new Headers({
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Headers': '*',
+	'Access-Control-Allow-Methods': '*',
+})
+
 // Handle requests
 {
 	const handler = async (request: Request): Promise<Response> => {
-		const response =
-			(await rootRequestHandler(request)) ||
-			(request.method === 'OPTIONS' ? new Response('ok') : new Response('route not found', { status: http.Status.NotFound }))
+		const response = await rootRequestHandler(request)
 
+		// handle 404s but always return ok to preflight requests
+		if (!response) {
+			if (request.method === 'OPTIONS') return new Response('ok', { headers: defaultHeaders })
+
+			return new Response('route not found', { status: http.Status.NotFound, headers: defaultHeaders })
+		}
+
+		// Don't try to add CORS to ws connections
 		if (new URL(request.url).pathname === '/connection') return response
 
-		const headers = new Headers({
-			'Access-Control-Allow-Origin': '*',
-			'Access-Control-Allow-Headers': '*',
-			'Access-Control-Allow-Methods': '*',
-		})
-		for (const [key, value] of response.headers.entries()) headers.set(key, value)
+		// Add CORS to all other responses
+		{
+			for (const [key, value] of response.headers.entries()) defaultHeaders.set(key, value)
 
-		return new Response(await response.blob(), { headers, status: response.status, statusText: response.statusText })
+			return new Response(await response.blob(), {
+				headers: defaultHeaders,
+				status: response.status,
+				statusText: response.statusText,
+			})
+		}
 	}
 
 	console.log(`Starting server at http://localhost:${PORT}`)
@@ -31,11 +45,12 @@ import './bypass/mod.ts'
 		port: PORT,
 		// deno-lint-ignore no-explicit-any
 		onError(error: any) {
-			if (error.isUserError) return new Response(error.message, { status: 406 })
+			if (error.isUserError) return new Response(error.message, { status: 406, headers: defaultHeaders })
 
 			console.log('internal server error:\n', error.message ? error.message : error)
 			console.log(error.stack)
-			return new Response('internal server error', { status: 500 })
+
+			return new Response('internal server error', { status: 500, headers: defaultHeaders })
 		},
 	})
 }
